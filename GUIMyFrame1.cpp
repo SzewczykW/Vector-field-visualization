2b4d1ea57de45fed031ecd26c438179aabf72976
natruszka <ola.michalska2002@gmail.com>
2023-06-10 03:16:18 +0200

    GUI prototype

---
+++ GUIMyFrame1.cpp
@@ -0,0 +1,103 @@
+#include "GUIMyFrame1.h"
+
+GUIMyFrame1::GUIMyFrame1( wxWindow* parent )
+:
+MyFrame1( parent )
+{
+
+}
+
+void GUIMyFrame1::MainPanelOnSize( wxSizeEvent& event )
+{
+// TODO: Implement MainPanelOnSize
+}
+
+void GUIMyFrame1::MainPanelOnUpdateUI( wxUpdateUIEvent& event )
+{
+// TODO: Implement MainPanelOnUpdateUI
+}
+
+void GUIMyFrame1::functionOnChoice( wxCommandEvent& event )
+{
+// TODO: Implement functionOnChoice
+}
+
+void GUIMyFrame1::a_scrollBarOnScroll( wxScrollEvent& event )
+{
+// TODO: Implement a_scrollBarOnScroll
+}
+
+void GUIMyFrame1::b_scrollBarOnScroll( wxScrollEvent& event )
+{
+// TODO: Implement b_scrollBarOnScroll
+}
+
+void GUIMyFrame1::x_startOnText( wxCommandEvent& event )
+{
+// TODO: Implement x_startOnText
+}
+
+void GUIMyFrame1::x_stopOnText( wxCommandEvent& event )
+{
+// TODO: Implement x_stopOnText
+}
+
+void GUIMyFrame1::y_startOnText( wxCommandEvent& event )
+{
+// TODO: Implement y_startOnText
+}
+
+void GUIMyFrame1::y_stopOnText( wxCommandEvent& event )
+{
+// TODO: Implement y_stopOnText
+}
+
+void GUIMyFrame1::z_startOnText( wxCommandEvent& event )
+{
+// TODO: Implement z_startOnText
+}
+
+void GUIMyFrame1::z_stopOnText( wxCommandEvent& event )
+{
+// TODO: Implement z_stopOnText
+}
+
+void GUIMyFrame1::divideOnText( wxCommandEvent& event )
+{
+// TODO: Implement divideOnText
+}
+
+void GUIMyFrame1::arrow_lengthOnScroll( wxScrollEvent& event )
+{
+// TODO: Implement arrow_lengthOnScroll
+}
+
+void GUIMyFrame1::auto_arrowsOnCheckBox( wxCommandEvent& event )
+{
+// TODO: Implement auto_arrowsOnCheckBox
+}
+
+void GUIMyFrame1::x_scrollBarOnScroll( wxScrollEvent& event )
+{
+// TODO: Implement x_scrollBarOnScroll
+}
+
+void GUIMyFrame1::y_scrollBarOnScroll( wxScrollEvent& event )
+{
+// TODO: Implement y_scrollBarOnScroll
+}
+
+void GUIMyFrame1::z_scrollBarOnScroll( wxScrollEvent& event )
+{
+// TODO: Implement z_scrollBarOnScroll
+}
+
+void GUIMyFrame1::addSurfaceOnCheckBox( wxCommandEvent& event )
+{
+// TODO: Implement addSurfaceOnCheckBox
+}
+
+void GUIMyFrame1::saveOnButtonClick( wxCommandEvent& event )
+{
+// TODO: Implement saveOnButtonClick
+}
